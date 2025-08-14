a88117c71cd0c4c5e2882415fce863eb351272a0
Rahib Jafarov <rahib.cefer.98@gmail.com>
2025-08-14 13:57:04 +0400

    Open API integration and changing to application.yml from application.properties

--- account/src/main/java/com/rahibjafar/account/AccountApplication.java
+++ account/src/main/java/com/rahibjafar/account/AccountApplication.java
@@ -1,8 +1,14 @@
 package com.rahibjafar.account;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(title = "Account API", version = "v1", description = "Xidmətin REST API-si")
+)
 @SpringBootApplication
 public class AccountApplication {
 	public static void main(String[] args) {
